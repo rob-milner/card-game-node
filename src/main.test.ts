b7e601bd7c365b081e9f2a8dfcdd6ff5200b6c7f
Rob Milner <rob.milner@salecycle.com>
2022-06-23 20:14:55 +0100

    add functions and tests to cover game logic

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,169 @@
-import { sum } from "./main";
+import {
+  newDeck,
+  shuffle,
+  deal,
+  drawCard,
+  compareScore,
+  awardPointToPlayer,
+  playRound,
+  Deck,
+  Players,
+} from "./main";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+const orderedDeck = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+];
+
+describe("newDeck", () => {
+  let deck: Deck;
+  beforeEach(() => {
+    deck = newDeck();
+  });
+
+  it("has the correct length", () => {
+    expect(deck).toHaveLength(52);
+  });
+
+  it("has the correct values", () => {
+    expect(deck).toStrictEqual(orderedDeck);
+  });
+});
+
+describe("shuffle", () => {
+  let deck: Deck;
+  beforeEach(() => {
+    deck = newDeck();
+  });
+
+  it("returns a deck of the same length", () => {
+    const shuffled = shuffle(deck);
+    expect(shuffled).toHaveLength(52);
+  });
+
+  it("mixes up the cards differently each time it is run", () => {
+    const shuffledHistory = [orderedDeck];
+
+    for (let i = 0; i < 10; i++) {
+      const shuffled = shuffle(deck);
+      const match = shuffledHistory.find(
+        (s) => s.toString() === shuffled.toString()
+      );
+      expect(match).toBe(undefined);
+      shuffledHistory.push(shuffled);
+    }
+  });
+});
+
+describe("deal", () => {
+  let p1Cards: Deck, p2Cards: Deck;
+
+  beforeEach(() => {
+    const deck = shuffle(newDeck());
+    [p1Cards, p2Cards] = deal(deck);
+  });
+
+  it("splits the deck between the two players", () => {
+    expect(p1Cards).toHaveLength(26);
+    expect(p2Cards).toHaveLength(26);
+  });
+
+  it("has unique cards for each player", () => {
+    const intersection = p1Cards.filter((card) => p2Cards.includes(card));
+    expect(intersection).toHaveLength(0);
+  });
+
+  it("includes the whole deck between both players", () => {
+    const combined = [...p1Cards, ...p2Cards].sort((a, b) => a - b);
+    expect(combined).toEqual(orderedDeck);
+  });
+});
+
+describe("drawCard", () => {
+  let startingDeck: Deck;
+
+  beforeEach(() => {
+    startingDeck = [1, 2, 3];
+  });
+
+  it("returns the value of the topmost card in the deck", () => {
+    const { card } = drawCard(startingDeck);
+    expect(card).toBe(3);
+  });
+
+  it("removes the topmost card from the deck", () => {
+    const { deck } = drawCard(startingDeck);
+    expect(deck).toEqual([1, 2]);
+  });
+
+  it("returns 0 and an empty deck when a deck is empty", () => {
+    const { card, deck } = drawCard([]);
+    expect(card).toBe(0);
+    expect(deck).toEqual([]);
+  });
+});
+
+describe("compareScore", () => {
+  it("returns the player number with the highest card", () => {
+    expect(compareScore(7, 5)).toBe(1);
+    expect(compareScore(3, 4)).toBe(2);
+  });
+});
+
+describe("awardPoint", () => {
+  it("increases the score of the winning player", () => {
+    let players = {
+      1: { name: "p1", score: 0, deck: [] },
+      2: { name: "p2", score: 0, deck: [] },
+    } as Players;
+
+    players = awardPointToPlayer(players, 1);
+
+    expect(players[1].score).toBe(1);
+    expect(players[2].score).toBe(0);
+
+    players = awardPointToPlayer(players, 2);
+    players = awardPointToPlayer(players, 2);
+
+    expect(players[1].score).toBe(1);
+    expect(players[2].score).toBe(2);
+  });
+});
+
+describe("playRound", () => {
+  let players: Players;
+
+  beforeEach(() => {
+    players = {
+      1: { name: "p1", score: 0, deck: [1, 2] },
+      2: { name: "p2", score: 0, deck: [3, 4] },
+    };
+  });
+
+  it("signals the game has not ended while there are still cards left to play", () => {
+    const { gameOver } = playRound(players);
+    expect(gameOver).toBe(false);
+  });
+
+  it("signals the game has ended when there are no more cards to play", () => {
+    const players = {
+      1: { name: "p1", score: 0, deck: [1] },
+      2: { name: "p2", score: 0, deck: [2] },
+    };
+    const { gameOver } = playRound(players);
+    expect(gameOver).toBe(true);
+  });
+
+  it("removes the topmost card after playing a round", () => {
+    const result = playRound(players);
+    expect(result.players[1].deck).toEqual([1]);
+    expect(result.players[2].deck).toEqual([3]);
+  });
+
+  it("awards points after playing a round", () => {
+    const result = playRound(players);
+    expect(result.players[1].score).toEqual(0);
+    expect(result.players[2].score).toEqual(1);
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,74 @@
 "use strict";
 
-export function sum(a: number, b: number): number {
-  return a + b;
-}
+const DECK_SIZE = 52;
 
-console.log(sum(1, 2));
+export type Deck = number[];
+
+type DrawCardResult = {
+  card: number;
+  deck: Deck;
+};
+
+type Player = {
+  name: string;
+  score: number;
+  deck: Deck;
+};
+
+export type Players = Record<number, Player>;
+
+type PlayRoundResult = {
+  players: Players;
+  gameOver: boolean;
+};
+
+export const newDeck = (): Deck => {
+  const deck: Deck = [];
+  for (let i = 1; i <= DECK_SIZE; i++) {
+    deck.push(i);
+  }
+  return deck;
+};
+
+export const shuffle = (deck: Deck): Deck => {
+  const copy = deck.slice(0);
+
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+
+  return copy;
+};
+
+export const deal = (deck: Deck): [Deck, Deck] => {
+  const half = Math.ceil(deck.length / 2);
+  return [deck.slice(0, half), deck.slice(-half)];
+};
+
+export const drawCard = (deck: Deck): DrawCardResult => {
+  const copy = [...deck];
+  return { card: copy.pop() ?? 0, deck: copy };
+};
+
+export const compareScore = (player1: number, player2: number): number => {
+  return player1 > player2 ? 1 : 2;
+};
+
+export const awardPointToPlayer = (players: Players, key: number): Players => {
+  const p = { ...players };
+  p[key].score++;
+  return p;
+};
+
+export const playRound = (players: Players): PlayRoundResult => {
+  let p = { ...players };
+  const p1Draw = drawCard(p[1].deck);
+  const p2Draw = drawCard(p[2].deck);
+  const winner = compareScore(p1Draw.card, p2Draw.card);
+  p = awardPointToPlayer(p, winner);
+  p[1].deck = p1Draw.deck;
+  p[2].deck = p2Draw.deck;
+  const gameOver = p[1].deck.length === 0;
+  return { players: p, gameOver };
+};
